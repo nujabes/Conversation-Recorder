72b1d080603c0692676850ace2c8acc649efe13d
nujabes <nujab2s@gmail.com>
2015-01-22 18:03:22 +0900

    change v.4
    Organize results.
    Clearing code and function.
    Implement prograss bar from bootstrap.

--- js/script.js
+++ js/script.js
@@ -1,102 +1,76 @@
-// v0.3
-// bootstrap ui 도입
-// button UI 도입
-// history 제거, 버튼 추가
-// button을 눌러 topic을 닫을수 있음
+// ver 0.4
+// result정리
+// 함수 정리
+// pograss bar 도입
+
 
 jQuery(document).ready(function($) {
 	var inputbox = $('input#console');
-	var history = $('#history').append('<p></p><p></p><p></p><p></p><p></p>');
 	var $result = $('#results');
 	var topics = [];
 
-	// var topic;
-		// $('.topicButtons').delicate('.stopRecord','click', function() {
-		// 	// this.finish();
-		// 	console.log(event);
-		// });
-		$('.topicButtons').on('click','.stopRecord', function() {
-			thisTopic = $(this).data('topic');
-			// console.log(thisTopic);
-			// topics['thisTopic'].finish();
-			for (var i in topics) {
-				if (topics[i].name == thisTopic) {
-					topics[i].finish();
+	window.onload = function(){
+		setInterval(function() {
+			if(topics.length > 0){
+				// console.log(topics[0].getDuration());
+				for (var i in topics) {
+					// topics[i].drawPrograssBar();
 					break;
 				}
 			}
-		});
+		}, 1000 / 5);
+	};
 
-	// $('.btn-default').on('click', function(){console.log('a')});
-		$('.inputEnter').click(function(event){
-			var e = jQuery.Event("keydown");
-			e.which = 13;
-			inputbox.trigger(e);
-		});
-	
+	$('.topicButtons').on('click', '.stopRecord', function() {
+		thisTopic = $(this).data('topic');
+		for (var i in topics) {
+			if (topics[i].name == thisTopic) {
+				topics[i].finish();
+				break;
+			}
+		}
+	});
+
+	$('.inputEnter').click(function(event) {
+		var e = jQuery.Event("keydown");
+		e.which = 13;
+		inputbox.trigger(e);
+	});
+
 
 	inputbox.on('keydown', function(e) {
 		if (e.which == 13 && inputbox.val() !== '') { // enter key
 			var input = inputbox.val();
-			processInput(input);
+			parseInput(input);
 			afterInput();
 		}
 	});
 
-	inputbox.on('keypress', function(e) {
-		if (e.which == 47) { // '/' type character
-			// var availableTags = [
-			// 	"/ActionScript",
-			// 	"/AppleScript",
-			// 	"/Asp",
-			// 	"/BASIC",
-			// 	"/C",
-			// 	"/C++",
-			// 	"/Clojure",
-			// 	"/COBOL",
-			// 	"/ColdFusion",
-			// 	"/Erlang",
-			// 	"/Fortran",
-			// 	"/Groovy",
-			// 	"/Haskell",
-			// 	"/Java",
-			// 	"/JavaScript",
-			// 	"/Lisp",
-			// 	"/Perl",
-			// 	"/PHP",
-			// 	"/Python",
-			// 	"/Ruby",
-			// 	"/Scala",
-			// 	"/Scheme"
-			// ];
-			// inputbox.autocomplete({
-			// 	source: availableTags
-			// });
-			// console.log('a');
-			// var input = inputbox.val();
-			// processInput(input);
-			// afterInput();
-		}
-	});
+	// inputbox.on('keypress', function(e) {
+	// 	if (e.which == 47) { // '/' type character
+	// 		var availableTags = [
+	// 			"/ActionScript",
+	// 			"/AppleScript",
+	// 			"/Asp",
+	// 			"/BASIC",
+	// 		];
+	// 		inputbox.autocomplete({
+	// 			source: availableTags
+	// 		});
+	// 		console.log('a');
+	// 		var input = inputbox.val();
+	// 		parseInput(input);
+	// 		afterInput();
+	// 	}
+	// });
 
-
-	var cleanResult = function(argument) {
-		if ($result.find('li').length > 5) {
-			// console.log('remove');
-			// console.log($result.find('li').eq(0));
-			$result.find('li').eq(0).remove();
-		}
-	};
-
-	var processInput = function(input) {
-		// result.append('<p>' + input + '</p>');
-
+	var parseInput = function(input) {
 		var openTag = input.match(/^([ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|\s|\-]*)$/);
 		var closeTag = input.match(/^[\/]([ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|\s|\-]*)$/);
 
 		if (openTag !== null) {
-			topics.push(new Topic(openTag[1], new Date()));
-			// topics[topics.length - 1].addBtn();
+			var topic = new Topic(openTag[1], new Date());
+			topics.push(topic);
 		} else if (closeTag !== null) {
 			var name = closeTag[1];
 			for (var i in topics) {
@@ -106,34 +80,20 @@ jQuery(document).ready(function($) {
 				}
 			}
 		}
-
-		// console.log(topics);
 	};
 
 
 
 	var afterInput = function(argument) {
 		if (topics.length > 0) {
-			var html = '';
-			for (var i in topics) {
-				html += topics[i].printLog();
+			var li = $result.find('li:first');
+
+			if ($result.find('p').length > 5) {
+				$('#results p:first').remove();
 			}
-			$result.html(html);
-			cleanResult();
 		}
 		inputbox.val('');
 	};
-
-
-
-	// $('.topicButtons').find('.stopRecord').each(function(index, el) {
-	// 	$(this).click(function(event) {
-	// 		// this.finish();
-	// 		console.log(event);
-	// 	});
-	// });
-
-
 
 
 	var Topic = function(title, created) {
@@ -144,27 +104,41 @@ jQuery(document).ready(function($) {
 			going;
 		this.name = title;
 		this.startTime = created;
-		this.addBtn();
+		this.duration = 0;
+		this.printResult();
+		this.drawButton();
+		this.drawPrograssBar();
 	};
 
 	Topic.prototype = {
-		printLog : function() {
+		printResult: function(finish) {
 			var html = '';
-			html += '<li><span class="title">' + this.name + '</span>';
-			html += '<span class="startTime">' + formatTime(this.startTime) + '</span>';
-			if (this.closeTime !== undefined) {
+			if(finish === undefined){
+				html += '<p><span class="title">' + this.name + '</span> begin';
+				html += '<span class="startTime">' + formatTime(this.startTime) + '</span>';
+			}else{
+				html += '<p><span class="title">' + this.name + '</span> end';
 				html += '<span class="closeTime">' + formatTime(this.closeTime) + '</span>';
 				html += '<span class="duration">' + formatMinute(this.duration) + '</span>';
 			}
-			html += '</li>';
-			return html;
-			// $result.append(html);
+			html += '</p>';
+			return $result.append(html);
 		},
-		addBtn : function(){
-			var btnhtml = '<div class="btn-group"><button type="button" class="btn btn-default btn-xs" disabled>'+this.name+'</button><button type="button" class="btn btn-default btn-xs stopRecord" aria-expanded="false" data-topic="'+this.name+'"><span class="glyphicon glyphicon-stop"></span></button></div>';
+		drawButton: function() {
+			var btnhtml = '<div class="btn-group"><button type="button" class="btn btn-default btn-xs" disabled>' + this.name + '</button><button type="button" class="btn btn-default btn-xs stopRecord" aria-expanded="false" data-topic="' + this.name + '"><span class="glyphicon glyphicon-stop"></span></button></div>';
 			$('.topicButtons').prepend(btnhtml);
 		},
-		finish : function() {
+		drawPrograssBar: function() {
+			var percentage = this.duration / 1200 * 1000;
+			var prograssBarHtml = '<div class="progress"><div class="progress-bar" role="progressbar" data-topic="' + this.name +'" aria-valuenow="'+ percentage +'" aria-valuemin="0" aria-valuemax="100" style="min-width:30px; width: '+ percentage +'%;">' + this.name + '</div></div>';
+			$('.barGroup').prepend(prograssBarHtml);
+		},
+		updatePrograssBar: function() {
+			var percentage = this.duration / 1200 * 1000;
+			var prograssBarHtml = '<div class="progress"><div class="progress-bar" role="progressbar" data-topic="' + this.name +'" aria-valuenow="'+ percentage +'" aria-valuemin="0" aria-valuemax="100" style="min-width:30px; width: '+ percentage +'%;">' + this.name + '</div></div>';
+			$('.barGroup').prepend(prograssBarHtml);
+		},
+		finish: function() {
 			this.closeTime = new Date();
 			this.duration = this.closeTime - this.startTime;
 			// for (var i in topics) {
@@ -174,17 +148,26 @@ jQuery(document).ready(function($) {
 			// 	}
 			// }
 			var btn = $('.topicButtons')
-			.find("[data-topic='"+this.name+"']");
+				.find("[data-topic='" + this.name + "']");
 			btn
-			.removeClass('btn-danger')
-			.attr("disabled",true)
+				.removeClass('btn-danger')
+				.attr("disabled", true)
 			// .addClass('btn-success')
 			.find('span')
-			.attr("disabled",true)
-			.removeClass('glyphicon-stop')
-			.addClass('glyphicon-ok');
+				.attr("disabled", true)
+				.removeClass('glyphicon-stop')
+				.addClass('glyphicon-ok');
 			// console.log(topics);
+
+			this.printResult(true);
 			afterInput();
+		},
+		getDuration: function(){
+			// var obj = [];
+			// obj.
+			// obj.dueration = new Date() - this.startTime;
+			this.duration = new Date() - this.startTime;
+			return this.duration;
 		}
 	};
 
@@ -198,13 +181,8 @@ var formatTime = function(time) {
 };
 
 var formatMinute = function(time) {
-	// var hour = time.getHours(),
-	// min = time.getMinutes(),
 	time = Math.floor(time / 1000);
-	// console.log(time);
 	var minutes = Math.floor(time / 60);
 	var seconds = time - minutes * 60;
-
-	// sec = time.getMilliseconds();
 	return minutes + 'min ' + seconds + 's';
 };
